a152af2b27ea3b387d7d58af7ac538015ad280e2
AsadRay <asaduzzamanrayhan15@gmail.com>
2025-02-18 13:34:19 +0600

    Second Lecture

---
+++ Lecture2.py
@@ -0,0 +1,31 @@
+a = int(42)
+b = int(42)
+
+if a == b:
+    print("Equal")
+else:
+    print("Not equal")
+    
+c = 'Hello'    
+d = 'hello'
+if c != d:
+    print("equal")
+else:
+    print("Not equal")
+    
+a = 'marry'
+b = '01749452202'
+d =input("Enter your Name-:")
+if d == a:
+    print("Name Matched!")
+else:
+    print("Wrong Name!!!")
+    print("Correct Name is",a)
+    print("Try to Login With Your Number As you forgot Your name!")
+    
+b='01749452202'
+c=input("Enter Your Number-")
+if b == c:
+    print("YOU ARE RIGHT!!!!!. LOGGED IN")
+else:
+    print("I think you are fruad")
