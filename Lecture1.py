af9c06d1eb2e1153349b54e0b75b077ea61d4c41
Asaduzzaman Rayhan <asaduzzamanrayhan15@gmail.com>
2025-02-18 10:24:02 +0600

    added Lecture1.py

---
+++ Lecture1.py
@@ -0,0 +1,18 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = int(input("Enter the third number: "))
+a = int(6)
+b = int(5)
+c = int(7)
+sum = a + b + c
+print("The sum of the numbers is:", sum)'''
+
+
+string1 = input("Enter the first string: ")
+string2 = input("Enter the second string: ")
+
+concatenated_string = string1 + string2
+
+print("The concatenated string is:", concatenated_string)
+print('Rayhan'*5)
+
